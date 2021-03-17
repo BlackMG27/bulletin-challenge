d29755a05c029d85e270f1021da10308e538c5e3
BlackMG27 <oike279@gmail.com>
2021-03-17 19:54:02 -0400

    got some of the setup done

---
+++ bulletin-challenge/src/App.js
@@ -0,0 +1,14 @@
+import {Fragment} from 'react';
+
+import './App.scss';
+
+function App() {
+  return (
+    <Fragment>
+      <h1>This is working</h1>
+    </Fragment>
+  );
+}
+
+
+export default App;
